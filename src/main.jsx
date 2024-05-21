05a1a97585e919d7f552cd4b8dfdf2bff88a48eb
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2024-05-21 16:52:55 -0700

    added backend code to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Setup AWS Amplify Backend
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
+
+Amplify.configure(outputs)
+
+// Use backend API
+import { generateClient } from 'aws-amplify/api'
+
+const client = generateClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
