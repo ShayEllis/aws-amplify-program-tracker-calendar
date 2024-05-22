cf60a1f6b90d5b4b85b606c9dc6a6f27c4508a09
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2024-05-22 11:00:46 -0700

    update build commands

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,15 @@ import './index.css'
 
 // Setup AWS Amplify Backend
 import { Amplify } from 'aws-amplify'
-// import outputs from '../amplify_outputs.json'
-// console.log(outputs)
+import outputs from '../amplify_outputs.json'
+console.log(outputs)
 
 // Amplify.configure(outputs)
 
 // // Use backend API
-// import { generateClient } from 'aws-amplify/api'
+import { generateClient } from 'aws-amplify/api'
 
-// const client = generateClient()
+const client = generateClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
