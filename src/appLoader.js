1d2c49d550dc4738b0f0cb71ddc3c9f54fa109f0
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2024-06-28 12:52:11 -0700

    adj usted body, root and bodyContainer css to fix scolling issue

--- src/appLoader.js
+++
@@ -1,9 +0,0 @@
-import { calendarServer } from './utils/calendarServer'
-
-const appLoader = () => {
-  calendarServer.fetchCalendarDayData().then((response) => {
-    return response
-  })
-}
-
-export { appLoader }
